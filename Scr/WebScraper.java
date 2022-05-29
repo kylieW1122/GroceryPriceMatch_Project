5e6967e9446403d07f3567d9e74292ad40acfddd
kylieW1122 <monkeykylie1122@gmail.com>
2022-05-28 23:30:02 -0400

    using Jsoup, successfully access the first 50 items in Sobey's website
    Need to be done:
    - Formatting of the Strings
    - decide the way we store data (itemName, price, Sale?)
    - access more than 50 items

--- Scr/WebScraper.java
+++ Scr/WebScraper.java
@@ -1,26 +1,56 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.List;
+import java.util.*;
 /**[WebScraper.java]
  * This is final project - price match program
- * This class ......
- * 
+ * This class gets the website's data and save it as......
+ * https://youtu.be/yw7B85174JQ 
  * @author Kylie Wong and Michelle Chan, ICS4UE
- * @version 1.0, build May 27, 2022
+ * @version 1.1, build May 27, 2022
  */
 public class WebScraper {
-
-    public static void main(String[] args) throws Exception {
-        final Document document = Jsoup.connect
-            ("https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=1810000201&cubeTimeFrame.startMonth=10&cubeTimeFrame.startYear=2021&cubeTimeFrame.endMonth=04&cubeTimeFrame.endYear=2022&referencePeriods=20211001%2C20220401").get();
-        //System.out.println(document.outerHtml()); //"http://www.imdb.com/chart/top"
-
-        for (Element row : document.select("table.pub-table.simpleTable dataTable tr")) { //"table.chart.full-width tr"
-            System.out.println("hello"); //class="pub-table simpleTable dataTable"
-            final String title = row.select(".highlight-row").text(); //.titleColumn a
-            //final String rating = row.select(".imdbRating").text();
-
-            System.out.println(title); //+ " -> Rating: " + rating);
-        }
+    private static LinkedHashMap<String, String> sobeysItemsMap = new LinkedHashMap<String, String>();
+    final static String SOBEYS_URL = "https://voila.ca/products?source=navigation";
+//----------------------------------------------------------------------------
+    public static void main(String[] args){
+        setUpSobeysList();
+    }
+//----------------------------------------------------------------------------
+    private static void setUpSobeysList(){
+        try{
+            final Document document = Jsoup.connect(SOBEYS_URL).get();
+            //System.out.println(document.outerHtml());
+            String title = document.title();
+            System.out.println("website: " + title + "\n");
+            
+            Elements test = document.select("div.layout__Container-sc-1cgl98j-0.laSkfV");
+            Element selectByTag = test.last();
+            /************************/
+            Elements tag = selectByTag.getElementsByTag("h3");
+            Elements prices = selectByTag.getElementsByTag("span");
+            String itemName = "";
+            for(Element tags: tag){
+                itemName = tags.text();
+                sobeysItemsMap.put(itemName, "");
+            }
+            System.out.println(sobeysItemsMap.toString() + "\n");
+            String spanTagString = "";
+            for(Element price: prices){
+                spanTagString = spanTagString + price.text();
+            }
+            
+            //System.out.println(spanTagString);
+            String[] arrOfStr = spanTagString.split("You have of this item in your cart");
+            int indexOfMap = 0;
+            for (String a : arrOfStr){
+                System.out.println(a);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        } 
     }
 }
