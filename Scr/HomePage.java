a1b4138d19052c0c0c4d58fda910e2c881ba6906
kylieW1122 <monkeykylie1122@gmail.com>
2022-05-30 09:04:30 -0400

    update, added classes

---
+++ Scr/HomePage.java
@@ -0,0 +1,13 @@
+/**[HomePage.java]
+ * This is final project - price match program
+ * This class contains the main method to start the program
+ * 
+ * @author Kylie Wong and Michelle Chan, ICS4UE
+ * @version 1.0, build May 27, 2022
+ */
+
+public class HomePage{
+    HomePage(){
+       
+    }
+}
