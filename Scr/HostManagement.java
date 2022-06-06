ed03488e588cfb389c87ab11198b83fa7ee6e34f
kylieW1122 <monkeykylie1122@gmail.com>
2022-06-06 08:47:45 -0400

    Added multi-user framework, renamed to HostManagement
    Use User as the starting of HomePage-GUI
    
    HostManagement as the serverHost

---
+++ Scr/HostManagement.java
@@ -0,0 +1,84 @@
+//package es.neil.networking;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.io.*;
+import java.net.*;
+/**[PriceMatchManagement.java]
+ * This is final project - price match program
+ * This class contains the main method and the host server to start the program
+ * 
+ * @author Kylie Wong and Michelle Chan, ICS4UE
+ * @version 1.0, build May 27, 2022
+ */
+
+public class HostManagement{
+    private HashMap<String, String> userHashMap = new HashMap<String, String>();
+    
+    final int PORT = 6666;
+    ServerSocket serverSocket;
+    Socket clientSocket;
+    PrintWriter output;
+    BufferedReader input;
+//----------------------------------------------------------------------------
+    public static void main(String[] args) throws Exception{
+        DataBase database = new DataBase();
+        HostManagement hostServer = new HostManagement();
+        hostServer.start();
+    }
+//----------------------------------------------------------------------------
+    public boolean registerUser(String id, String password){
+        if(userHashMap.containsKey(id)){
+            return false; //false - userId taken 
+        }else{
+            userHashMap.put(id,password);
+        }
+        return true;
+    }
+//----------------------------------------------------------------------------
+    private void start() throws Exception{ 
+        //create a socket with the local IP address and wait for connection request       
+        System.out.println("Waiting for a connection request from a client ...");
+        serverSocket = new ServerSocket(PORT);                //create and bind a socket
+        while(true){
+            clientSocket = serverSocket.accept();             //wait for connection request
+            System.out.println("Client connected");
+            Thread connectionThread = new Thread(new ConnectionHandler(clientSocket));
+            connectionThread.start();                         //start a new thread to handle the connection
+        }
+    }
+//----------------------------------------------------------------------------
+    public boolean userLogin(String userId){
+        return false;
+    }
+//------------------------------------------------------------------------------
+    class ConnectionHandler extends Thread { 
+        Socket socket;
+        PrintWriter output;
+        BufferedReader input;
+        
+        public ConnectionHandler(Socket socket) { 
+            this.socket = socket;
+        }
+        @Override 
+        public void run() {
+            try {
+                input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                output = new PrintWriter(socket.getOutputStream());
+                //receive a message from the client
+                String msg = input.readLine();
+                System.out.println("Message from the client: " + msg);
+                //send a response to the client
+                output.println("Client you are connected!");
+                output.flush();         
+                while((msg = input.readLine())!=null){
+                    System.out.println(msg);
+                }
+                //after completing the communication close the streams but do not close the socket!
+                input.close();
+                output.close();
+            }catch (IOException e) {e.printStackTrace();}
+        }
+    }  
+//------------------------------------------------------------------------------
+}
--- Scr/User.java
+++ Scr/User.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
 /**[User.java]
  * This is final project - price match program
  * This class represent the object - User
@@ -8,14 +12,89 @@
 public class User{
     private String password;
     private String userID;
+    private static ArrayList<String> requestGroupOrderList;
+    private ArrayList<String> committedGroupOrderList;
+    
+    final String LOCAL_HOST = "127.0.0.1";
+    final int PORT = 6666;
+    Socket clientSocket;
+    PrintWriter output;    
+    BufferedReader input;
+    public static void main(String[] args){
+        
+        //start homepage here, for user
+        User user = new User("jel", "ds");
+        Scanner userInput = new Scanner(System.in);
+        String line = userInput.nextLine();
+        while(!line.equals("bye")){
+            if(line.equals("login")){
+                user.userLogin("", "");
+            }else{
+                user.createGroupOrder(line);
+            }
+            line = userInput.nextLine();
+        }
+    }
 //----------------------------------------------------------------------------
     User(String id, String password){
+        this.requestGroupOrderList = new ArrayList<String>();
         this.userID = id;
         this.password = password;
     }
 //----------------------------------------------------------------------------
     public String getUserID(){ return userID; }
 //----------------------------------------------------------------------------
+    public void createGroupOrder(String requestInfo){
+        if(clientSocket.isConnected()){
+            output.println("grpOrder - "+ this.getUserID() + "," +requestInfo);               //send the requestInfo to the host
+            output.flush();
+            this.requestGroupOrderList.add(requestInfo);
+            System.out.println("here: " + requestGroupOrderList.toString());
+        }
+    }
+/****************************************************************************/
+//Networking
+//----------------------------------------------------------------------------
+    public boolean userLogin(String id, String password){
+        return this.startConnecting();
+    }
+//----------------------------------------------------------------------------
+    public boolean userLogout(){
+        return this.stopConnecting();
+    }
+//----------------------------------------------------------------------------
+    private boolean startConnecting(){
+        try{
+            System.out.println("Attempting to establish a connection ...");
+            clientSocket = new Socket(LOCAL_HOST, PORT);          //create and bind a socket, and request connection
+            output = new PrintWriter(clientSocket.getOutputStream());
+            input = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            System.out.println("Connection to server established!");
+            //testing - delete after
+            output.println("Hi. I am a basic client!");           //send a message to the server
+            output.flush();                                       //ensure the message was sent but not kept in the buffer
+            String msg = input.readLine();                        //get a response from the server
+            System.out.println("Message from server: '" + msg+"'"); 
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+//----------------------------------------------------------------------------
+    private boolean stopConnecting(){
+        try{
+            input.close();
+            output.close();
+            clientSocket.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+//----------------------------------------------------------------------------
+/****************************************************************************/
     @Override
     public String toString(){ return this.userID; }
 //----------------------------------------------------------------------------
